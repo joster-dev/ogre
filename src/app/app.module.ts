d042bb817b3e2aac01aa65ae689127dbbc9af920
Joster422 <Joster422@gmail.com>
2020-03-12 20:10:37 -0400

    chore - gh-pages deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { environment } from '../environments/environment';
       'ngsw-worker.js',
       {
         enabled: environment.production,
+        scope: './',
         registrationStrategy: 'registerImmediately'
       }
     )
